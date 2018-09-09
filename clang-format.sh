d95b4a3868ad74d8601f36e948f85a2c2ee2d59d
v-zhuravlev <zhuravlev.vitaly@gmail.com>
2018-09-09 22:09:42 +0300

    synced master, resolved conflits

---
+++ clang-format.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+clang-format -i --style=file src/*.c
---
+++ run-tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [[ ${TRAVIS_JOB_TYPE} == "FORMAT" ]]; then
+   echo "Going to do clang-format check..."
+   ./clang-format.sh
+   git diff --exit-code
+else 
+    # run tests
+    echo "Going to build and then test with docker..."
+    ./configure --enable-zabbix-${ZBX_HEADERS_VERSION}
+    make && sudo make install
+    pushd tests/docker && docker-compose up -d
+    popd
+    docker logs docker_zabbix-agent-modbus_1
+    docker exec docker_zabbix-agent-modbus_1 sh -c "zabbix_get -s localhost -k agent.ping"
+    docker exec docker_zabbix-agent-modbus_1 sh -c "zabbix_get -s localhost -k modbus_read[172.16.238.2:5020,1,2,3,uint16,1,1]"
+    pytest -v
+fi
