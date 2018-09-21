fc6369e459a6e99b9f4a0d83e45678202f3f8a2e
v-zhuravlev <zhuravlev.vitaly@gmail.com>
2018-09-21 16:54:00 +0300

    added new tests when where datatype is not provided

--- tests/test_modbus_datatype_errors.py
+++ tests/test_modbus_datatype_errors.py
@@ -48,18 +48,35 @@ class ModbusDatatypeErrorsTestCase(unittest.TestCase):
         key = "modbus_read_registers["+self.host+",1,2,3,"+formula+"]"
         self.assertEqual(zabbix_get(key), 'ZBX_NOTSUPPORTED: Invalid type in the datatype expression.')
     
-    def test_modbus_no_datatype_for_read_registers(self):
-        """This test checks that if no datatype is provided then 'uint16' must be used for functions 3 and 4"""
+    def test_modbus_no_datatype_for_read_registers_0(self):
+        """This test checks that if no datatype is provided(with ',') then 'uint16' must be used for functions 3 and 4"""
         formula = ""
         key = "modbus_read_registers["+self.host+",1,2,3,"+formula+"]"
         self.assertEqual(zabbix_get(key), '36802')
     
+    def test_modbus_no_datatype_for_read_registers_1(self):
+        """This test checks that if no datatype is provided then 'uint16' must be used for functions 3 and 4"""
+        key = "modbus_read_registers["+self.host+",1,2,3]"
+        self.assertEqual(zabbix_get(key), '36802')
+
+    def test_modbus_no_datatype_for_read_registers_2(self):
+        """This test checks that if no datatype is provided then 'uint16' must be used for functions 3 and 4"""
+        key = "modbus_read_registers["+self.host+",1,2,3,,1,0]"
+        self.assertEqual(zabbix_get(key), '36802')
+    
     @unittest.skip("Implement this first")
-    def test_modbus_no_datatype_for_read_coils(self):
-        """This test checks that if no datatype is provided then 'bit' must be used for functions 1 and 2"""
+    def test_modbus_no_datatype_for_read_coils_0(self):
+        """This test checks that if no datatype is provided(with ',') then 'bit' must be used for functions 1 and 2"""
         formula = ""
         key = "modbus_read_registers["+self.host+",1,2,1,"+formula+"]"
-        self.assertEqual(zabbix_get(key), 'some coil value')        
+        self.assertEqual(zabbix_get(key), 'some coil value')
+
+    @unittest.skip("Implement this first")
+    def test_modbus_no_datatype_for_read_coils_1(self):
+        """This test checks that if no datatype is provided then 'bit' must be used for functions 1 and 2"""
+        formula = ""
+        key = "modbus_read_registers["+self.host+",1,2,1]"
+        self.assertEqual(zabbix_get(key), 'some coil value')
 
     # test that 'bit' is not supported:
     def test_modbus_datatype_expression_bit_not_allowed(self):
