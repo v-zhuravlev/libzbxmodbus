0041a50542e81530f385fb5742be7a057545ef8d
v-zhuravlev <zhuravlev.vitaly@gmail.com>
2018-09-24 09:26:33 +0300

    parametrized 32bit tests

--- tests/test_03modbus.py
+++ tests/test_03modbus.py
@@ -52,45 +52,31 @@ class TestModbus(object):
         key = "modbus_read[{},1,0,{},float]".format(host, modbus_func)
         assert zabbix_get(key) == '-71.879005'
 
-    @pytest.mark.parametrize(("reg", "endianess", "expected"), [
-        ("0", "BE", "-71.879005"),
-        ("4", "MLE", "-71.879005"),
-        ("2", "MBE", "-71.879005"),
-        ("6", "LE", "-71.879005")
-    ], ids=["BE", "MLE", "MBE", "LE"])
-    def test_modbus_datatype_float_32bit(self, host, modbus_func, reg, endianess, expected):
-        key = "modbus_read[{},1,{},{},float,{}]".format(
-            host, reg, modbus_func, endianess)
-        assert zabbix_get(key) == expected
-
     # UINT32,unsigned,long:
     def test_modbus_datatype_long_32bit_default(self, host, modbus_func):
         key = "modbus_read[{},1,0,{},uint32]".format(host, modbus_func)
         assert zabbix_get(key) == '3264201229'
-
-    @pytest.mark.parametrize(("reg", "endianess", "expected"), [
-        ("0", "BE", "3264201229"),
-        ("0", "MLE", "3255681679"),
-        ("0", "MBE", "2411859394"),
-        ("0", "LE", "230854594")
-    ], ids=["BE", "MLE", "MBE", "LE"])
-    def test_modbus_datatype_long_32bit(self, host, modbus_func, reg, endianess, expected):
-        key = "modbus_read[{},1,{},{},uint32,{}]".format(
-            host, reg, modbus_func, endianess)
-        assert zabbix_get(key) == expected
 
     # INT32(Signed)
     def test_modbus_datatype_int32_default(self, host, modbus_func):
         key = "modbus_read[{},1,12,{},int32]".format(host, modbus_func)
         assert zabbix_get(key) == '-562.000000'
 
-    @pytest.mark.parametrize(("reg", "endianess", "expected"), [
-        ("12", "BE", "-562.000000"),
-        ("12", "MLE", "-36765697.000000"),
-        ("12", "MBE", "-12547.000000"),
-        ("12", "LE", "-822214657.000000")
-    ], ids=["BE", "MLE", "MBE", "LE"])
-    def test_modbus_datatype_int32_32bit(self, host, modbus_func, reg, endianess, expected):
-        key = "modbus_read[{},1,{},{},int32,{}]".format(
-            host, reg, modbus_func, endianess)
+    @pytest.mark.parametrize(("reg", "datatype", "endianess",  "expected"), [
+        ("0", "float", "BE", "-71.879005"),
+        ("4", "float", "MLE", "-71.879005"),
+        ("2", "float", "MBE", "-71.879005"),
+        ("6", "float", "LE", "-71.879005"),
+        ("0", "uint32", "BE", "3264201229"),
+        ("0", "uint32", "MLE", "3255681679"),
+        ("0", "uint32", "MBE", "2411859394"),
+        ("0", "uint32", "LE", "230854594"),
+        ("12", "int32", "BE", "-562.000000"),
+        ("12", "int32", "MLE", "-36765697.000000"),
+        ("12", "int32", "MBE", "-12547.000000"),
+        ("12", "int32", "LE", "-822214657.000000")
+    ], ids=["float-BE", "float-MLE", "float-MBE", "float-LE", "uint32-BE", "uint32-MLE", "uint32-MBE", "uint32-LE", "int32-BE", "int32-MLE", "int32-MBE", "int32-LE"])
+    def test_modbus_datatype_32bit(self, host, modbus_func, reg, datatype, endianess,  expected):
+        key = "modbus_read[{},1,{},{},{},{}]".format(
+            host, reg, modbus_func, datatype, endianess)
         assert zabbix_get(key) == expected
