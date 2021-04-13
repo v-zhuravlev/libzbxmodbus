6c04f42695b14ad4492f149f427b02605948993a
i-ky <gl.ivanovsky@gmail.com>
2021-04-13 21:05:28 +0000

    Revert back to concatenation since f-strings are not supported in Travis CI

--- tests/zabbix_get.py
+++ tests/zabbix_get.py
@@ -1,6 +1,6 @@
 import subprocess
 
 def zabbix_get(key):
-    batchcmd = f"docker-compose exec zabbix-agent-modbus sh -c \"zabbix_get -s localhost -k{key}\""
+    batchcmd = "docker-compose exec zabbix-agent-modbus sh -c \"zabbix_get -s localhost -k" + key + "\""
     result = subprocess.check_output(batchcmd, shell=True, cwd="tests/docker/")
     return result.rstrip()
