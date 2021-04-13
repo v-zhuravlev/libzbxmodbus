e09654644960edfbdd5d84e467e01239cfc3bd1c
i-ky <gl.ivanovsky@gmail.com>
2021-04-13 21:43:32 +0000

    Provide absolute path to docker-compose directory

--- tests/zabbix_get.py
+++ tests/zabbix_get.py
@@ -1,6 +1,8 @@
 import subprocess
+from os.path import abspath, dirname, join
 
 def zabbix_get(key):
     batchcmd = "docker-compose exec zabbix-agent-modbus sh -c \"zabbix_get -s localhost -k" + key + "\""
-    result = subprocess.check_output(batchcmd, shell=True, cwd="tests/docker/")
+    directory = join(dirname(abspath(__file__)), "tests", "docker")
+    result = subprocess.check_output(batchcmd, shell=True, cwd=directory)
     return result.rstrip()
