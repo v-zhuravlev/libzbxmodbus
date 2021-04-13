b3a40e50ef335d67e67f867dac4adfbb6db65623
i-ky <gl.ivanovsky@gmail.com>
2021-04-13 22:02:10 +0000

    Fix path

--- tests/zabbix_get.py
+++ tests/zabbix_get.py
@@ -3,6 +3,6 @@ from os.path import abspath, dirname, join
 
 def zabbix_get(key):
     batchcmd = "docker-compose exec zabbix-agent-modbus sh -c \"zabbix_get -s localhost -k" + key + "\""
-    directory = join(dirname(abspath(__file__)), "tests", "docker")
+    directory = join(dirname(abspath(__file__)), "docker")
     result = subprocess.check_output(batchcmd, shell=True, cwd=directory)
     return result.rstrip()
